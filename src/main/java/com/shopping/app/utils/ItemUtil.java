15d555a35977c45d1778ce73276dc4521879c86f
baynescorps <baynescorps@googlemail.com>
2018-11-09 16:19:04 +0000

    Added utils for creation of Items and Promotions

---
+++ src/main/java/com/shopping/app/utils/ItemUtil.java
@@ -0,0 +1,18 @@
+package com.shopping.app.utils;
+
+import com.shopping.app.model.Item;
+
+/**
+ * Created by baynescorps on 09/11/2018.
+ */
+public class ItemUtil {
+
+    /*This class only really makes sense in the context of this test as an easy way to fetch the items on offer and
+    share them with the tests. In the real world we'd probably fetch each item individually from a service*/
+
+    public static Item headPhones = new Item("Audio","Headphones",150.00);
+    public static Item speakers = new Item("Audio","Speakers",85);
+    public static Item batteries = new Item("Power", "AAA Batteries", 0.85);
+    public static Item proteinBars = new Item("Food", "Food", 0.85);
+
+}
---
+++ src/main/java/com/shopping/app/utils/PromotionUtil.java
@@ -0,0 +1,30 @@
+package com.shopping.app.utils;
+
+import com.shopping.app.enums.MatchType;
+import com.shopping.app.impl.DiscountPromotion;
+import com.shopping.app.impl.XForXPromotion;
+import com.shopping.app.interfaces.Promotion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by baynescorps on 09/11/2018.
+ */
+public class PromotionUtil {
+
+    /*This class only really makes sense in the context of this test as an easy way to fetch the promotions and
+    share them with the tests without duplicating code. In the real world we'd probably fetch each promotion
+    individually from a service*/
+
+    public static Promotion thirtyPercentPromotion = new DiscountPromotion(30.00, "Audio", MatchType.ProductType);
+    public static Promotion threeforTwoPromotion = new XForXPromotion(3,2,"AAA Batteries", MatchType.Name);
+
+    public static List<Promotion> getPromotionList()
+    {
+        List<Promotion> promotionList = new ArrayList<>();
+        promotionList.add(thirtyPercentPromotion);
+        promotionList.add(threeforTwoPromotion);
+        return promotionList;
+    }
+}
